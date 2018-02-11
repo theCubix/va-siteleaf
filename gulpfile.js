b3cfae7c0375f5fe3431d680d61e4fb2950725b5
[thecubix] <[timon.forrer@gmail.com]>
2018-02-11 22:19:21 +0100

    Added minification to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css')
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./css'));
 });
  
