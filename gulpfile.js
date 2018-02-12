c2944578c2606f2a345b0cdd577cdf7e6d60e2fa
[thecubix] <[timon.forrer@gmail.com]>
2018-02-12 18:32:28 +0100

    changed url to sass files

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sources/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('optimize-css', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sources/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 4 versions'],
@@ -23,5 +23,5 @@ gulp.task('optimize-css', function () {
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sources/sass/**/*.scss', ['sass']);
 });
