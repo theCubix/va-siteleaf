9226c8b159ee9c06bdf110f3b60cfc209efc9a15
[thecubix] <[timon.forrer@gmail.com]>
2018-04-15 11:32:36 +0200

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var eslint = require('gulp-eslint');
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style='color: grey'>Running:</span> $ jekyll build'
 };
 
 /**
@@ -28,7 +32,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['sass', 'js', 'jekyll-build'], function() {
   browserSync({
     server: {
       baseDir: '_site'
@@ -40,7 +44,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from sources/sass into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-  return gulp.src('./sources/sass/styles.scss')
+  return gulp.src('sources/sass/styles.scss')
     .pipe(sass({
       includePaths: ['scss'],
       onError: browserSync.notify
@@ -54,13 +58,45 @@ gulp.task('sass', function () {
 /**
  * Compile files from sources/js into both _site/js (for live injecting) and site (for future jekyll builds)
  */
+gulp.task('js', function () {
+  return gulp.src('sources/js/**/*.js')
+  .pipe(eslint({
+    'envs': [
+      { 'es6': true }
+    ],
+    'extends': 'eslint:recommended',
+    'rules': {
+      'indent': ['error', 4],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'double'],
+      'semi': ['error', 'always'],
+
+      // override default options for rules from base configurations
+      'comma-dangle': ['error', 'always'],
+      'no-cond-assign': ['error', 'always'],
+
+      // disable rules from base configurations
+      'no-console': 'off'
+    }
+  }))
+  .pipe(eslint.format())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('_site/js'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('js'));
+})
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-  gulp.watch('./sources/sass/**/*.scss', ['sass']);
+  gulp.watch('sources/sass/**/*.scss', ['sass']);
+  gulp.watch('sources/js/**/*.js', ['js']);
   gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes'], ['jekyll-rebuild']);
 });
 
